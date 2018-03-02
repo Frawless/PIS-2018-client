eff0adee35809fd7beea482fa2026079f6c410ee
Jakub Stejskal <stejskinek@gmail.com>
2018-03-02 19:33:33 +0100

    NEW: login routine added to login.ts, fieldIsEmpty check added

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,20 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { routerTransition } from '../router.animations';
+
+import { AlertService, AuthenticationService } from '../_authentication/_services/index';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
-    animations: [routerTransition()]
+    animations: [routerTransition()],
+
+    moduleId: module.id.toString(),
 })
 export class LoginComponent implements OnInit {
-    constructor(public router: Router) {}
+    model: any = {};
+    loading = false;
+    returnUrl: string;
+    title = 'Bakery';
 
-    ngOnInit() {}
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService) { }
+
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
 
     onLoggedin() {
         localStorage.setItem('isLoggedin', 'true');
     }
-    title = 'Bakery';
+
+    login() {
+        console.log(this.returnUrl)
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+
 }
