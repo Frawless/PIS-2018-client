7765685819ebd1a1bb24b9b9e1f94d97e7afb204
Jakub Stejskal <stejskinek@gmail.com>
2018-03-02 12:52:51 +0100

    NEW: Login and signup dialog added to shop component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -7,12 +7,16 @@ import { ShopRoutingModule } from "./shop-routing.module";
 import { ShopComponent } from './shop.component';
 import { HeaderComponent } from './components/header/header.component';
 
+import { DialogComponent } from '../dialog/dialog.component';
+import { LoginComponent} from '../login/login.component';
+import { SignupComponent } from '../signup/signup.component';
+
 @NgModule({
   imports: [
       CommonModule,
       ShopRoutingModule,
       NgbModule.forRoot()
   ],
-  declarations: [ShopComponent, HeaderComponent]
+  declarations: [ShopComponent, HeaderComponent, DialogComponent, LoginComponent, SignupComponent]
 })
 export class ShopModule {}
