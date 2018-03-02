3e589c88145ee3d0bbc1269203bf9639cb17edf4
Jakub Stejskal <stejskinek@gmail.com>
2018-03-02 12:52:25 +0100

    FIX: app-routing syntax error fixed

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
     { path: 'shop', loadChildren: './shop/shop.module#ShopModule' },
     { path: 'admin', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
     { path: 'signup', loadChildren: './signup/signup.module#SignupModule' },
-    { path: 'login', loadChildren: './login/login.module#LoginModule' }
+    { path: 'login', loadChildren: './login/login.module#LoginModule' },
     { path: 'access-denied', loadChildren: './access-denied/access-denied.module#AccessDeniedModule' },
     { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
     { path: '**', redirectTo: 'not-found' }
