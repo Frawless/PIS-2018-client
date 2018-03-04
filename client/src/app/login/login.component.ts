f584261abb5ae412109f4f337caeccccf21de367
Jakub Stejskal <stejskinek@gmail.com>
2018-03-04 12:01:30 +0100

    NEW: Login updated for share window with signup, validators added

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { routerTransition } from '../router.animations';
 
 import { AlertService, AuthenticationService } from '../_authentication/_services/index';
+import {UserService} from "../_authentication/_services/user.service";
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-login',
@@ -13,40 +15,110 @@ import { AlertService, AuthenticationService } from '../_authentication/_service
     moduleId: module.id.toString(),
 })
 export class LoginComponent implements OnInit {
-    model: any = {};
-    loading = false;
+    @Input() showLogin = true;
+
+    credentials: any = {};
+    registration: any = {};
+    loginLoading = false;
+    registrationLoading = false;
     returnUrl: string;
     title = 'Bakery';
+
+    // showLogin: boolean = true;
+    showLoginButton: boolean = true;
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
         private authenticationService: AuthenticationService,
-        private alertService: AlertService) { }
+        private userService: UserService,
+        private alertService: AlertService,
+        private formBuilder: FormBuilder) {
+    }
 
+    // @TODO check this
+    // https://loiane.com/2017/08/angular-reactive-forms-trigger-validation-on-submit/
     ngOnInit() {
         // reset login status
         this.authenticationService.logout();
+
+        // Login validators
+        this.credentials = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+
+        })
+        // Registration validators
+        this.registration = this.formBuilder.group({
+            firstName: ['', [Validators.required, Validators.maxLength(40)]],
+            lastName: ['', [Validators.required, Validators.maxLength(40)]],
+            username: ['', [Validators.required, Validators.maxLength(40), Validators.minLength(4)]],
+            email: ['', [Validators.email, Validators.required]],
+            password: ['', Validators.required]
+            // @TODO jstejska: add min-length validator for password and regex validator for names
+
+        })
+
         // get return url from route parameters or default to '/'
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
-    onLoggedin() {
-        localStorage.setItem('isLoggedin', 'true');
-    }
-
     login() {
-        console.log(this.returnUrl)
-        this.loading = true;
-        this.authenticationService.login(this.model.username, this.model.password)
+        // Form validity check
+        if(!this.credentials.valid){
+            this.alertService.error('Login form is not valid');
+            return;
+        }
+
+        this.loginLoading = true;
+
+        this.authenticationService.login(this.credentials.username, this.credentials.password)
             .subscribe(
                 data => {
-                    this.router.navigate([this.returnUrl]);
+                    this.router.navigate([this.router.url]);
+                    this.showLogin = false;
+                    this.showLoginButton = false;
                 },
                 error => {
                     this.alertService.error(error);
-                    this.loading = false;
+                    this.loginLoading = false;
                 });
     }
 
+    register() {
+        // Form validity check
+        if(!this.registration.valid){
+            this.alertService.error('Registration form is not valid');
+            return;
+        }
+
+        this.registrationLoading = true;
+
+        // Submit - not working now
+        this.userService.create(this.registration)
+            .subscribe(
+                data => {
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate([this.router.url]);
+                    this.registrationLoading = false;
+                    this.registration.reset();
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.registrationLoading = false;
+                });
+    }
+
+    logout() {
+        this.showLogin = false;
+        this.showLoginButton = true;
+        this.loginLoading = false;
+        this.credentials.reset();
+        this.registration.reset();
+    }
+
+    showDialogLogin() {
+        this.showLogin = true;
+        console.log(this.showLogin);
+    }
 }
