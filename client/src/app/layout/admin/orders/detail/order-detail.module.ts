1beb72299762727863e3178872d57aad0207d8f8
zatlik <zatliktom@gmail.com>
2018-04-17 13:21:23 +0200

    UPD: make table row link to product detail

--- client/src/app/layout/admin/orders/detail/order-detail.module.ts
+++ client/src/app/layout/admin/orders/detail/order-detail.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {OrderDetailComponent} from './order-detail.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
+        RouterModule
         // OrderDetailComponent
     ],
     exports: [
